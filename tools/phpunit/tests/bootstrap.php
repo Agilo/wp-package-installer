d4dd3f22d3331d42c58708ea748e888745b537b0
Ivo Limić <ilimic1@gmail.com>
2023-09-04 11:53:07 +0200

    impoving how TEST_PROJECT_ROOT_DIR is outputed.

--- tools/phpunit/tests/bootstrap.php
+++ tools/phpunit/tests/bootstrap.php
@@ -6,6 +6,6 @@ define('PROJECT_ROOT_DIR', realpath(__DIR__.'/../../..'));
 define('TEST_PROJECT_ROOT_DIR', sys_get_temp_dir().'/wp-package-installer-test-project');
 define('TESTS_ROOT_DIR', realpath(__DIR__.'/..'));
 
-var_dump('TEST_PROJECT_ROOT_DIR', TEST_PROJECT_ROOT_DIR);
+echo 'Using ['.TEST_PROJECT_ROOT_DIR.'] for TEST_PROJECT_ROOT_DIR.'.PHP_EOL;
 
 require_once __DIR__ . '/../vendor/autoload.php';
