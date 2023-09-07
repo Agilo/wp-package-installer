0e7cbf8ad6f6de616f18630792569c1fabdcf9a6
Ivo Limić <ilimic1@gmail.com>
2023-09-07 10:42:55 +0200

    moving code around.

--- tools/phpunit/tests/BuildTest.php
+++ tools/phpunit/tests/BuildTest.php
@@ -10,14 +10,6 @@ use Symfony\Component\Process\Process;
 
 class BuildTest extends TestCase
 {
-    public function vanillaBuildDataProvider()
-    {
-        return [
-            [true, 'composer-symlinked-build.json'],
-            [false, 'composer-non-symlinked-build.json'],
-        ];
-    }
-
     private function setupProject(string $composerJsonFilename): void
     {
         /**
@@ -46,77 +38,6 @@ class BuildTest extends TestCase
         $process = new Process(['composer', 'install'], TEST_PROJECT_ROOT_DIR);
         $process->setTimeout(5 * 60);
         $this->assertSame(0, $process->run(), $process->getCommandLine().' failed with the error below.'.PHP_EOL.$process->getErrorOutput());
-    }
-
-    /**
-     * @dataProvider vanillaBuildDataProvider
-     */
-    public function testVanillaBuild(bool $isSymlinkedBuild, string $composerJsonFilename): void
-    {
-        if ($isSymlinkedBuild && DIRECTORY_SEPARATOR === '\\') {
-            $this->markTestSkipped('Symlinked builds are not supported on Windows');
-        }
-
-        $firstPartySrc = TEST_PROJECT_ROOT_DIR.'/src';
-        $thirdPartySrc = TEST_PROJECT_ROOT_DIR.'/vendor';
-        $dest = TEST_PROJECT_ROOT_DIR.'/public';
-
-        $this->setupProject($composerJsonFilename);
-        $this->assertProjectFiles($isSymlinkedBuild, $firstPartySrc, $thirdPartySrc, $dest);
-
-        /**
-         * test updating WP core
-         */
-        $process = new Process(['composer', 'require', '--update-with-dependencies', 'johnpbloch/wordpress:6.2.2'], TEST_PROJECT_ROOT_DIR);
-        $process->setTimeout(5 * 60);
-        $this->assertEquals(0, $process->run(), $process->getCommandLine().' failed with the error below.'.PHP_EOL.$process->getErrorOutput());
-
-        $this->assertProjectFiles($isSymlinkedBuild, $firstPartySrc, $thirdPartySrc, $dest);
-
-        /**
-         * test updating existing wpackagist-plugin/<plugin>
-         */
-
-        // update wpackagist-plugin/wp-crontrol 1.14 => 1.15
-        $process = new Process(['composer', 'require', 'wpackagist-plugin/wp-crontrol:1.15'], TEST_PROJECT_ROOT_DIR);
-        $process->setTimeout(5 * 60);
-        $this->assertEquals(0, $process->run(), $process->getCommandLine().' failed with the error below.'.PHP_EOL.$process->getErrorOutput());
-
-        $this->assertProjectFiles($isSymlinkedBuild, $firstPartySrc, $thirdPartySrc, $dest);
-
-        /**
-         * test installing new wpackagist-plugin/<plugin>
-         */
-
-        $process = new Process(['composer', 'require', 'wpackagist-plugin/duplicate-post'], TEST_PROJECT_ROOT_DIR);
-        $process->setTimeout(5 * 60);
-        $this->assertEquals(0, $process->run(), $process->getCommandLine().' failed with the error below.'.PHP_EOL.$process->getErrorOutput());
-
-        // check that the plugin installed successfully
-        $isLink = is_link($dest.'/wp-content/plugins/duplicate-post');
-        $this->assertSame($isLink, $isSymlinkedBuild);
-        $this->assertDirectoryExists($dest.'/wp-content/plugins/duplicate-post');
-        CustomAsserts::assertDirectoryNotEmpty($dest.'/wp-content/plugins/duplicate-post');
-        $this->assertFileEquals($thirdPartySrc.'/wp-content/plugins/duplicate-post/duplicate-post.php', $dest.'/wp-content/plugins/duplicate-post/duplicate-post.php');
-
-        // check that the new plugin install didn't break other files
-        $this->assertProjectFiles($isSymlinkedBuild, $firstPartySrc, $thirdPartySrc, $dest);
-
-        /**
-         * test uninstalling wpackagist-plugin/<plugin>
-         */
-
-        $process = new Process(['composer', 'remove', 'wpackagist-plugin/duplicate-post'], TEST_PROJECT_ROOT_DIR);
-        $process->setTimeout(5 * 60);
-        $this->assertEquals(0, $process->run(), $process->getCommandLine().' failed with the error below.'.PHP_EOL.$process->getErrorOutput());
-
-        // check that the plugin was deleted successfully
-        $this->assertFalse(is_link($dest.'/wp-content/plugins/duplicate-post'));
-        $this->assertFileNotExists($dest.'/wp-content/plugins/duplicate-post');
-        $this->assertDirectoryNotExists($dest.'/wp-content/plugins/duplicate-post');
-
-        // check that the plugin uninstall didn't break other files
-        $this->assertProjectFiles($isSymlinkedBuild, $firstPartySrc, $thirdPartySrc, $dest);
     }
 
     private function assertProjectFiles(bool $isSymlinkedBuild, string $firstPartySrc, string $thirdPartySrc, string $dest): void
@@ -191,6 +112,85 @@ class BuildTest extends TestCase
         $this->assertDirectoryExists($dest.'/wp-content/plugins/redirection');
         CustomAsserts::assertDirectoryNotEmpty($dest.'/wp-content/plugins/redirection');
         $this->assertFileEquals($thirdPartySrc.'/wp-content/plugins/redirection/redirection.php', $dest.'/wp-content/plugins/redirection/redirection.php');
+    }
+
+    public function vanillaBuildDataProvider()
+    {
+        return [
+            [true, 'composer-symlinked-build.json'],
+            [false, 'composer-non-symlinked-build.json'],
+        ];
+    }
+
+    /**
+     * @dataProvider vanillaBuildDataProvider
+     */
+    public function testVanillaBuild(bool $isSymlinkedBuild, string $composerJsonFilename): void
+    {
+        if ($isSymlinkedBuild && DIRECTORY_SEPARATOR === '\\') {
+            $this->markTestSkipped('Symlinked builds are not supported on Windows');
+        }
+
+        $firstPartySrc = TEST_PROJECT_ROOT_DIR.'/src';
+        $thirdPartySrc = TEST_PROJECT_ROOT_DIR.'/vendor';
+        $dest = TEST_PROJECT_ROOT_DIR.'/public';
+
+        $this->setupProject($composerJsonFilename);
+        $this->assertProjectFiles($isSymlinkedBuild, $firstPartySrc, $thirdPartySrc, $dest);
+
+        /**
+         * test updating WP core
+         */
+        $process = new Process(['composer', 'require', '--update-with-dependencies', 'johnpbloch/wordpress:6.2.2'], TEST_PROJECT_ROOT_DIR);
+        $process->setTimeout(5 * 60);
+        $this->assertEquals(0, $process->run(), $process->getCommandLine().' failed with the error below.'.PHP_EOL.$process->getErrorOutput());
+
+        $this->assertProjectFiles($isSymlinkedBuild, $firstPartySrc, $thirdPartySrc, $dest);
+
+        /**
+         * test updating existing wpackagist-plugin/<plugin>
+         */
+
+        // update wpackagist-plugin/wp-crontrol 1.14 => 1.15
+        $process = new Process(['composer', 'require', 'wpackagist-plugin/wp-crontrol:1.15'], TEST_PROJECT_ROOT_DIR);
+        $process->setTimeout(5 * 60);
+        $this->assertEquals(0, $process->run(), $process->getCommandLine().' failed with the error below.'.PHP_EOL.$process->getErrorOutput());
+
+        $this->assertProjectFiles($isSymlinkedBuild, $firstPartySrc, $thirdPartySrc, $dest);
+
+        /**
+         * test installing new wpackagist-plugin/<plugin>
+         */
+
+        $process = new Process(['composer', 'require', 'wpackagist-plugin/duplicate-post'], TEST_PROJECT_ROOT_DIR);
+        $process->setTimeout(5 * 60);
+        $this->assertEquals(0, $process->run(), $process->getCommandLine().' failed with the error below.'.PHP_EOL.$process->getErrorOutput());
+
+        // check that the plugin installed successfully
+        $isLink = is_link($dest.'/wp-content/plugins/duplicate-post');
+        $this->assertSame($isLink, $isSymlinkedBuild);
+        $this->assertDirectoryExists($dest.'/wp-content/plugins/duplicate-post');
+        CustomAsserts::assertDirectoryNotEmpty($dest.'/wp-content/plugins/duplicate-post');
+        $this->assertFileEquals($thirdPartySrc.'/wp-content/plugins/duplicate-post/duplicate-post.php', $dest.'/wp-content/plugins/duplicate-post/duplicate-post.php');
+
+        // check that the new plugin install didn't break other files
+        $this->assertProjectFiles($isSymlinkedBuild, $firstPartySrc, $thirdPartySrc, $dest);
+
+        /**
+         * test uninstalling wpackagist-plugin/<plugin>
+         */
+
+        $process = new Process(['composer', 'remove', 'wpackagist-plugin/duplicate-post'], TEST_PROJECT_ROOT_DIR);
+        $process->setTimeout(5 * 60);
+        $this->assertEquals(0, $process->run(), $process->getCommandLine().' failed with the error below.'.PHP_EOL.$process->getErrorOutput());
+
+        // check that the plugin was deleted successfully
+        $this->assertFalse(is_link($dest.'/wp-content/plugins/duplicate-post'));
+        $this->assertFileNotExists($dest.'/wp-content/plugins/duplicate-post');
+        $this->assertDirectoryNotExists($dest.'/wp-content/plugins/duplicate-post');
+
+        // check that the plugin uninstall didn't break other files
+        $this->assertProjectFiles($isSymlinkedBuild, $firstPartySrc, $thirdPartySrc, $dest);
     }
 
     public function testSymlinkedBuildWithCustomPaths()
