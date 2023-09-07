ecfdc1892c5cbafeef2320ffa613facedcfb453a
Ivo Limić <ilimic1@gmail.com>
2023-09-07 09:58:56 +0200

    fixing tmp dir name.

--- tools/phpunit/tests/bootstrap.php
+++ tools/phpunit/tests/bootstrap.php
@@ -3,7 +3,7 @@
 namespace Agilo\WpPackageInstaller\Tests;
 
 define('PROJECT_ROOT_DIR', realpath(__DIR__.'/../../..'));
-define('TEST_PROJECT_ROOT_DIR', sys_get_temp_dir().'/wp-package-installer-test-project');
+define('TEST_PROJECT_ROOT_DIR', sys_get_temp_dir().'/agilo-wp-package-installer-test-project');
 define('TESTS_ROOT_DIR', realpath(__DIR__.'/..'));
 
 echo 'Using ['.TEST_PROJECT_ROOT_DIR.'] for TEST_PROJECT_ROOT_DIR.'.PHP_EOL;
